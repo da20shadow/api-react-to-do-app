1e6614f61dddb7d1f75c4df9ed93c033e4b8ad6b
vipmehmed <vipmehmed99@gmail.com>
2022-06-04 19:51:41 +0300

    htaccess

--- api/users/user.php
+++ api/users/user.php
@@ -1,2 +1,26 @@
 <?php
-echo "USERS DATA";
+echo "USERS DATA <br>";
+echo "<br>";
+echo "URL:";
+
+$url = $_SERVER['REQUEST_URI'];
+
+var_dump($url);
+echo "<br>";
+echo "<hr>";
+echo "<br>";
+
+if (preg_match("/\//",$url)){
+
+    $users = [
+        ["id" => 1, "username" => "Admin", "email" => "admin@email.com"],
+        ["id" => 2, "username" => "John", "email" => "john@email.com"],
+        ["id" => 3, "username" => "Peter", "email" => "peter@email.com"],
+    ];
+    echo "<pre>";
+    var_dump($users);
+    echo "</pre>";
+}
+else if (preg_match("/\/user/",$url)){
+    echo "GET ONE USER";
+}
